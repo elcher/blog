be198170d0310d5dc4a7baeb3c1dfb18e773cadb
Longcan Huang <elcheur@gmail.com>
2019-09-12 12:03:30 -0500

    removed posts, drafts, and site from the remote

--- _posts/2019-09-12-relations.md
+++
@@ -1,5 +0,0 @@
----
-layout: post
-title:  relations
-date:   2019-09-12 11:09:59 -0500
----
--- _site/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git checkout gh-pages
-git add -A
-git commit -m "Update blog"
-git push origin master
